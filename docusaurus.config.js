e9296ea66f94586bbc7e3649d35374ad56f35218
Gowthamguna01 <gowthamguna46@gmail.com>
2025-09-26 15:32:06 +0530

    Changes in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-           routeBasePath: 'doc',
+           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
