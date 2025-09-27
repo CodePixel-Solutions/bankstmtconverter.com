8cb673fcb2f12febade7ac113afaff5d3d88b7fe
Gowthamguna01 <gowthamguna46@gmail.com>
2025-09-27 19:02:01 +0530

    Changes in baseurl and routeBasePath  docusaurus.config.js & remove /docs in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,6 @@ import { Redirect } from '@docusaurus/router';
 
 export default function Home() {
   // Redirect user to the docs root, which will show the sidebar and first doc
-  return <Redirect to="/docs/intro" />;   //directly open the intro page
+  return <Redirect to="/intro" />;   //directly open the intro page
 }
 
